09b1d32072b3191dc75a3af8fb3bb9f42f681022
JoeNicasio123 <joenicasio123@gmail.com>
2025-03-09 13:48:44 -0500

    calculator layout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    @IBOutlet weak var secondNumber: UITextField!
+    
+    @IBOutlet weak var answer: UILabel!
+    
+    
+    @IBAction func multi(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
